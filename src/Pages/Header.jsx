3c68684bc0ad4fdf5d9fbf4028cce3addbc000c7
ExtremeDevelopments <najarvis05@gmail.com>
2021-08-20 12:47:32 -0500

    woah

---
+++ src/Pages/Header.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+function Header() {
+    return (
+        <div className="App">
+            <p className="App-paragraph">Hurricane Tracker™</p>
+        </div>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Pages/App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Pages/Header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
